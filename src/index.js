13202ee2b3ee105d28293bcabe17ee935da07df3
egassam <lovetogivepain@gmail.com>
2026-09-06 23:19:30 -0500

    Bump Worker to 2.3.14 for styled meet results

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import schools from './schools.json';
 
-const VERSION='2.3.13';
+const VERSION='2.3.14';
 const HEADERS={
   'User-Agent':`Mozilla/5.0 (compatible; SAS-Sports/${VERSION}; Cloudflare-Worker)`,
   'Accept':'text/html,application/xhtml+xml'
