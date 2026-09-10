e175f2948b77587deb4a4d6111c8d80de03cf4d3
egassam <lovetogivepain@gmail.com>
2026-09-10 18:34:22 -0500

    Add Baylor and BYU to certified rollout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import schools from './schools.json';
 import {rosterSocialInstagrams} from './roster-socials.js';
 
-const VERSION='4.8.0';
+const VERSION='4.9.0';
 const FEED_FRESH_MS=25*1000;
 const FEED_STALE_MS=24*60*60*1000;
 const HEADERS={
@@ -47,6 +47,10 @@ const SPORT_PATHS={
 };
 const COMBINED_TEAM_SPORTS=new Set(['Basketball','Swimming & Diving']);
 const KNOWN_ROSTER_URLS=new Map(Object.entries({
+  'byu|Cross Country':['https://byucougars.com/sports/mens-cross-country/roster','https://byucougars.com/sports/womens-cross-country/roster'],
+  'byu|Soccer':'https://byucougars.com/sports/womens-soccer/roster',
+  'byu|Volleyball':'https://byucougars.com/sports/womens-volleyball/roster',
+  'byu|Football':'https://byucougars.com/sports/football/roster',
   'oklahoma-state|Cross Country':'https://okstate.com/sports/mxct/roster',
   'oklahoma-state|Track & Field':'https://okstate.com/sports/mxct/roster'
 }));
@@ -103,6 +107,14 @@ const KNOWN_URLS=new Map(Object.entries({
   'texas-tech|Cross Country':'https://texastech.com/sports/cross-country/schedule',
   'texas-tech|Track & Field':'https://texastech.com/sports/track-and-field/schedule',
   'texas-tech|Football':'https://texastech.com/sports/football/schedule',
+  'baylor|Cross Country':'https://baylorbears.com/sports/cross-country/schedule',
+  'baylor|Soccer':'https://baylorbears.com/sports/soccer/schedule',
+  'baylor|Volleyball':'https://baylorbears.com/sports/womens-volleyball/schedule',
+  'baylor|Football':'https://baylorbears.com/sports/football/schedule',
+  'byu|Cross Country':'https://byucougars.com/sports/womens-cross-country/schedule',
+  'byu|Soccer':'https://byucougars.com/sports/womens-soccer/schedule',
+  'byu|Volleyball':'https://byucougars.com/sports/womens-volleyball/schedule',
+  'byu|Football':'https://byucougars.com/sports/football/schedule',
   'nebraska|Volleyball':'https://huskers.com/sports/volleyball/schedule?view=list',
   'nebraska|Soccer':'https://huskers.com/sports/soccer/schedule',
   'nebraska|Cross Country':'https://huskers.com/sports/cross-country/schedule/season/2026',
